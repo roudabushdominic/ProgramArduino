da1cafb418affe23910260e62e59be3b732caa5f
Dominic <roudabushcs@gmail.com>
2022-02-24 09:42:30 -0800

    Double Blink using 25 milliseconds

--- Blink/Blink/Blink.ino
+++ Blink/Blink/Blink.ino
@@ -32,12 +32,12 @@ void setup() {
 // the loop function runs over and over again forever
 void loop() {
   digitalWrite(9, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
+  delay(25);                       // wait for a second
   digitalWrite(9, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+  delay(25);                       // wait for a second
   
   digitalWrite(8, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
+  delay(25);                       // wait for a second
   digitalWrite(8, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+  delay(25);                       // wait for a second
 }
