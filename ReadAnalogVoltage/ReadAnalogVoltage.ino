fafc81dd72cf853ba2b66fb6c27a306ce6a5e48e
Dominic <roudabushcs@gmail.com>
2022-02-28 09:49:51 -0800

    ReadAnalogVoltage Stable

--- ReadAnalogVoltage/ReadAnalogVoltage.ino
+++ ReadAnalogVoltage/ReadAnalogVoltage.ino
@@ -10,6 +10,9 @@
   https://www.arduino.cc/en/Tutorial/BuiltInExamples/ReadAnalogVoltage
 */
 
+//programmer: Dominic Roudabush 
+//Date: 2/28/2022
+
 // the setup routine runs once when you press reset:
 void setup() {
   // initialize serial communication at 9600 bits per second:
