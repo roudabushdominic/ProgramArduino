50ba836502719c2f59e6e4d73b436bda30596804
Dominic <roudabushcs@gmail.com>
2022-03-03 09:09:33 -0800

    Updated name and date info

--- UltraSonic/UltraSonic.ino
+++ UltraSonic/UltraSonic.ino
@@ -1,3 +1,8 @@
+//programmer: Dominic Roudabush
+//date: 3/3/2022
+//resource: https://create.arduino.cc/projecthub/knackminds/how-to-measure-distance-using-ultrasonic-sensor-hc-sr04-a-b9f7f8
+
+
 void setup() {
   // put your setup code here, to run once:
 
